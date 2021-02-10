d55f09f2a35d898e09938ff63e4e52727083a611
Stanislav Potemkin <woodger@ya.ru>
2021-02-10 10:36:30 +0300

    Added Typescript to CI/CD process

--- dist/recurse-io.d.ts
+++
@@ -1,8 +0,0 @@
-declare const _default: {
-    chmod(src: string, mode: number, callback: (err: Error) => void): void;
-    chown(src: string, uid: number, gid: number, callback: (err: Error) => void): void;
-    copy(src: string, dir: string, umask: number, callback: (err: Error) => void): void;
-    remove(src: string, callback: (err: Error) => void): void;
-    mkdir(dir: string, umask: number, callback: (err: Error) => void): void;
-};
-export default _default;
