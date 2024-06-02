d004afbe48412036017a610cf80c3a5faf8e0e6e
Stanislav Potemkin <woodger@ya.ru>
2024-06-02 17:56:06 +0300

    Changed chown arguments and applied fs.rm recursive function

--- lib/src/recurse-io.d.ts
+++ lib/src/recurse-io.d.ts
@@ -1,17 +1,7 @@
 /// <reference types="node" />
-import fs from 'node:fs';
-export type Files = Array<string>;
-export type NoParamCallback = fs.NoParamCallback;
-declare function chmod(src: string, mode: number, callback: NoParamCallback): void;
-declare function chown(src: string, uid: number, gid: number, callback: NoParamCallback): void;
-declare function copy(src: string, dir: string, umask: number, callback: NoParamCallback): void;
-declare function remove(src: string, callback: NoParamCallback): void;
-declare function mkdir(dir: string, umask: number, callback: NoParamCallback): void;
-declare const _default: {
-    chmod: typeof chmod;
-    chown: typeof chown;
-    copy: typeof copy;
-    remove: typeof remove;
-    mkdir: typeof mkdir;
-};
-export default _default;
+import { NoParamCallback } from 'node:fs';
+export declare function chmod(src: string, mode: number, callback: NoParamCallback): void;
+export declare function chown(src: string, uid: number, gid: number, callback: NoParamCallback): void;
+export declare function copy(src: string, dir: string, umask: number, callback: NoParamCallback): void;
+export declare function remove(src: string, callback: NoParamCallback): void;
+export declare function mkdir(dir: string, umask: number, callback: NoParamCallback): void;
